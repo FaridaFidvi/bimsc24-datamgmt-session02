b62bc024c96f84f3680f93c8a5bab4f7965228ec
FaridaFidvi <farida.fidvi@students.iaac.net>
2024-01-25 12:28:45 +0530

    Added the bootcamp challenge work

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,17 @@ import { ref } from "vue" // you need this to use ref()
 
 <!-- the template is where the html code goes -->
 <template>
+ <div id="navbar" class="container">  
+        <div id="title">My Personal website</div>
+       
+    </div>
+
+    <div id="flex">
+
+        <div id="sidebar" class="container"> Input </div>
+
+        <div id="main" class="container"> Output </div>
+    </div>
 
 
 
@@ -19,5 +30,62 @@ import { ref } from "vue" // you need this to use ref()
 <!-- style is where the css code goes -->
 <style scoped>
 
+html{
+    
+    background-color: rgb(59, 59, 59); 
+    color: white;
+}
 
+body{
+
+    margin:0;
+}
+
+div{
+
+    /* put the borders in the inside of container */
+    box-sizing: border-box;
+}
+
+#navbar{
+
+    height: 50px;
+    border-color: red;
+background-color: palevioletred;
+
+}
+
+#flex{
+    
+    display: flex;
+    height: calc(100vh - 50px);
+}
+
+#sidebar{
+    
+    width:20%;
+    border-color: blue;
+background-color: white;
+color: black;
+}
+
+#main{
+    width:80%;
+    border-color: green;
+background-color: lightblue
+
+
+}
+
+#title{
+    width: 70%;
+    float:left;
+}
+
+.container{
+
+    border-style: dotted;
+    border-width: 1px;
+
+}
 </style>
